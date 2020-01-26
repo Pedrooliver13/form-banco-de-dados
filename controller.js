e940c00d8497aa7f436f077cdd1c5b15df3cf918
Pedro Henrique de Oliveira <56042296+Pedrooliver13@users.noreply.github.com>
2020-01-26 17:21:37 -0300

    Create controller.js
    arquivo responsável por entregar as respostas e requisições(e um pouco de tratamento de info)!

---
+++ controller.js
@@ -0,0 +1,52 @@
+const { age , date } = require('../../lib/utils')//aui é logica de idade
+const Instructors = require('../models/instructors')
+
+module.exports = {
+    index(req, res){
+        Instructors.all(function(instructors){
+            return res.render('instructors/index' , { instructors })
+        })
+    },
+    show(req , res){
+       Instructors.find(req.params.id , function(instructor){
+            if(!instructor) return res.send("Instructors is not found")
+
+            instructor.age = age(instructor.birth)
+            instructor.services = instructor.services.split(',')
+            instructor.create_at = Intl.DateTimeFormat('pt-BR').format(instructor.create_at)
+
+           return res.render('instructors/show' , { instructor })
+       })
+    },
+    create(req , res){
+        return res.render('instructors/create')
+    },
+    post(req , res){
+        const keys = Object.keys(req.body)
+
+        for(const key of keys){
+            if(req.body[key] == "") return res.send('Please, fill all fields!')
+        }
+
+       Instructors.create(req.body , (instructor)=>{
+           return res.redirect(`/instructors/${ instructor.id }`)
+       })
+    },
+    edit(req, res) {
+        Instructors.find(req.params.id, (instructor) =>{
+            instructor.birth = date(instructor.birth).iso
+            
+            return res.render('instructors/edit' , { instructors: instructor })
+        })
+    },
+    put(req, res) {
+        Instructors.update(req.body , ()=>{
+            return res.redirect(`instructors/${ req.body.id }`)
+        })
+    },
+    delete(req, res){
+        Instructors.delete(req.body.id , ()=>{
+            return res.redirect('/instructors')
+        })
+    }
+}
